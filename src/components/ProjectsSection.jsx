b3f3842f171516319a3cf975eb24ea87a48000ac
Chayan Maji <chayanmaji@gmail.com>
2025-12-03 22:49:06 -0500

    Project section was updated

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -3,33 +3,13 @@ import { ArrowRight, ExternalLink, Github } from "lucide-react";
 const projects = [
   {
     id: 1,
-    title: "SaaS Landing Page",
-    description: "A beautiful landing page app using React and Tailwind.",
-    image: "/projects/project1.png",
-    tags: ["React", "TailwindCSS", "Supabase"],
-    demoUrl: "#",
-    githubUrl: "#",
-  },
-  {
-    id: 2,
-    title: "Orbit Analytics Dashboard",
-    description:
-      "Interactive analytics dashboard with data visualization and filtering capabilities.",
-    image: "/projects/project2.png",
-    tags: ["TypeScript", "D3.js", "Next.js"],
-    demoUrl: "#",
-    githubUrl: "#",
-  },
-  {
-    id: 3,
-    title: "E-commerce Platform",
-    description:
-      "Full-featured e-commerce platform with user authentication and payment processing.",
-    image: "/projects/project3.png",
-    tags: ["React", "Node.js", "Stripe"],
-    demoUrl: "#",
-    githubUrl: "#",
-  },
+    title: "Hotel Booking App - Landing Page",
+    description: "A beautiful landing page app using HTML, CSS and SCSS.",
+    image: "/projects/Trillo.png",
+    tags: ["Html", "CSS", "SCSS"],
+    demoUrl: "https://chayanmaji.github.io/Trillo/",
+    githubUrl: "https://github.com/chayanmaji/Trillo",
+  }
 ];
 
 export const ProjectsSection = () => {
@@ -100,7 +80,7 @@ export const ProjectsSection = () => {
           <a
             className="cosmic-button w-fit flex items-center mx-auto gap-2"
             target="_blank"
-            href="https://github.com/machadop1407"
+            href="https://github.com/chayanmaji"
           >
             Check My Github <ArrowRight size={16} />
           </a>
